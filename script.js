6ba2b968e9a9385d3589024d1ab48c53b8b01518
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-01-29 23:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	let newStr = str.toLowerCase();
+	let ans = newStr.split("").reverse().join("");
+	ans == newStr ? alert(true) : alert(false);
+	 // alert(newStr);
 
 }
+palindrome(prompt()); 
 module.exports = palindrome
